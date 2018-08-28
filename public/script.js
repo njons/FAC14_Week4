ce1ea0ce1e5892976b3a3644d58bb81759d56a28
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-08-28 21:08:42 +0100

    finalised functionality

--- public/script.js
+++ public/script.js
@@ -1,21 +1,19 @@
+// target your dom elements globally so you can access them from anywhere in the code
 var input = document.getElementById("search");
 var button = document.getElementById("button");
 var datalist = document.getElementById("datalist");
 
-// // create an object ready to hold the final filtered object with rearch results
-// var globalObj = {};
-
-// send xhr request to the server for each letter typed by the user
+// listen for every key press (collect the input and use it to send an xhr request to the server)
 input.addEventListener("keyup", function(event) {
   // get the input from the user
   var searchInput = input.value;
-  console.log("this is the search input from the user", searchInput);
+  // console.log("this is the search input from the user", searchInput);
 
-  // make sure there is input from the user
   if (searchInput) {
     // if there is input, make the request call
     let pageUrl = window.location.href;
-    xhrRequest(urlCreator(pageUrl, searchInput), createResultArrayCb);
+    // combine the current url with the input and how to handle the response once its back (cb in the xhr function)
+    xhrRequest(urlCreator(pageUrl, searchInput), constructDataList);
   }
 });
 
@@ -23,7 +21,7 @@ input.addEventListener("keyup", function(event) {
 function urlCreator(url, str) {
   // takes url and adds a string value that corresponds to the user search input (str)
   var searchUri = encodeURIComponent(str);
-  console.log("this is the url from the user", searchUri);
+  // console.log("this is the url from the user", searchUri);
   return url + "search/" + searchUri;
 }
 
@@ -36,12 +34,11 @@ function xhrRequest(url, cb) {
       // ensure that the object returned form the server is parsed
       var parsedObj = JSON.parse(xhr.responseText);
       // transfer the results into the empty object waiting for info
-      // globalObj = parsedObj;
-      console.log("this is the parsed obj", parsedObj);
+      // console.log("this is the parsed obj", parsedObj);
       return cb(parsedObj);
     } else if (xhr.readyState === 4 && xhr.status != 200) {
       // logs error if request gor all the way, but a status code other than 200 is returned
-      console.log("you did not get a status code of 200");
+      return "you did not get a status code of 200";
     }
   };
   xhr.open("GET", url, true);
@@ -49,35 +46,30 @@ function xhrRequest(url, cb) {
 }
 
 // here is were we handle the JSON
-function createResultArrayCb(obj) {
-  console.log("this is the returning obj:", obj);
-  var array = createLimitedArray(obj);
-  console.log("this is array:", array);
-  constructDataList(array);
-  console.log("create results array got exported!");
-}
+function constructDataList(resultsArray) {
+  // console.log("this is array in contructDataList:", resultsArray);
+  // clean list items at each keypress
+  datalist.innerText = "";
 
-function constructDataList(array) {
-  var lists = "";
-  array.forEach(function(item) {
-    var list = document.createElement("li");
-    list.value = item.question;
+  // make a list item for each question in the obj
+  resultsArray.forEach(function(item) {
+    var listItem = document.createElement("li");
+    listItem.innerText = item.question;
+    datalist.appendChild(listItem);
   });
 
-  datalist.appendChild(list);
+  // limit the number of list items at 5
+  if (resultsArray.length > 5) {
+    resultsArray = resultsArray.slice(0, 5);
+  } else if (resultsArray.length === 0) {
+    // if object is empty
+    resultsArray.push("No matches found");
+  }
+  return resultsArray;
 }
 
-function createLimitedArray(obj) {
-  var limitedArray = obj.question;
-  if (limitedArray.length > 5) {
-    limitedArray = limitedArray.slice(0, 5);
-  } else if (limitedArray.length === 0) {
-    // if object is empty
-    limitedArray.push("No matches found");
-  }
-  return limitedArray;
-}
-//
+// make sure that the same functions run at sumbit as well
 button.addEventListener("click", function(event) {
-  // event.preventDefault();
+  event.preventDefault();
+  xhrRequest(urlCreator(window.location.href, input.value), constructDataList);
 });
